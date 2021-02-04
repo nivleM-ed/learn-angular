41c464be0722f22a56dde9337b9ca1f1f4d6be06
Melvin Edward <46630582+nivleM-ed@users.noreply.github.com>
2021-02-04 21:18:33 +0800

    Functional hero component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,20 +1,24 @@
   import { Component, OnInit } from '@angular/core';
-  import { Hero } from '../hero.interface';
+  import { Hero } from '../interface/hero.interface';
+  import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
+
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  }
+  heroes = HEROES;
+  selectedHero: Hero = HEROES[0];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
 }
