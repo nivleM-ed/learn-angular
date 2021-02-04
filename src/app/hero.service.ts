a4ddd47540c18241f545d09925e8cbb7fff62dfe
Melvin Edward <46630582+nivleM-ed@users.noreply.github.com>
2021-02-04 22:09:03 +0800

    Added hero service

---
+++ src/app/hero.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { Hero } from './interface/hero.interface';
+import { HEROES } from './mock-heroes';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+
+  constructor() { }
+
+  getHeroes(): Hero[] {
+    return HEROES;
+  }
+}
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
-  import { Component, OnInit } from '@angular/core';
-  import { Hero } from '../interface/hero.interface';
-  import { HEROES } from '../mock-heroes';
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../interface/hero.interface';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,16 +9,23 @@
 })
 
 export class HeroesComponent implements OnInit {
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
 
-  constructor() { }
+  constructor(private heroService: HeroService) {
+
+  }
 
   ngOnInit(): void {
+    this.getHeroes();
   }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
   }
 
 }
